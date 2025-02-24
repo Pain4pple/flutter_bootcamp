4a3969dce84ac1e5cb889d64d9d947b92afd139f
Lara Nicole Gatchalian <105093357+Pain4pple@users.noreply.github.com>
2025-02-24 10:22:05 +0800

    Meals App - Animation

--- Meals App/meals/lib/screens/categories.dart
+++ Meals App/meals/lib/screens/categories.dart
@@ -5,27 +5,57 @@ import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
 
-  final List<Meal> availableMeals; 
+  final List<Meal> availableMeals;
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals =
-        availableMeals
+        widget.availableMeals
             .where((meal) => meal.categories.contains(category.id))
             .toList();
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(meals: filteredMeals, title: category.title),
+        builder:
+            (ctx) => MealsScreen(meals: filteredMeals, title: category.title),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return  GridView(
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
         padding: EdgeInsets.all(20),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -38,10 +68,24 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context,category);
+                _selectCategory(context, category);
               },
             ),
         ],
-      );
+      ),
+      builder:
+          (context, child) => SlideTransition(
+            position: Tween(
+              begin: const Offset(0, 0.3),
+              end: const Offset(0, 0),
+            ).animate(
+              CurvedAnimation(
+                parent: _animationController,
+                curve: Curves.easeInOutCirc,
+              ),
+            ),
+            child: child,
+          ),
+    );
   }
 }
